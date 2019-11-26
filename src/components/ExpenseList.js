02f4bfbae1740dfabda6c4d2814054bfdb4c624f
Amanat Juwel <amanatjuwel@gmail.com>
2019-11-26 12:55:10 +0600

    styles & babel-polyfill added

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -1,24 +1,35 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import ExpenseListItem from './ExpenseListItem';
-import selectExpense from '../selectors/expenses';
+import selectExpenses from '../selectors/expenses';
 
-//simple react component
-const ExpenseList = (props) => (
-  <div>
-    <h1>Expense List</h1>
-    {props.expenses.map((expense) => {
-      return <ExpenseListItem key={expense.id} {...expense} />
-    })}
+export const ExpenseList = (props) => (
+  <div className="content-container">
+    <div className="list-header">
+      <div className="show-for-mobile">Expenses</div>
+      <div className="show-for-desktop">Expense</div>
+      <div className="show-for-desktop">Amount</div>
+    </div>
+    <div className="list-body">
+      {
+        props.expenses.length === 0 ? (
+          <div className="list-item list-item--message">
+            <span>No expenses</span>
+          </div>
+        ) : (
+            props.expenses.map((expense) => {
+              return <ExpenseListItem key={expense.id} {...expense} />;
+            })
+          )
+      }
+    </div>
   </div>
 );
 
-//preparing what to get from store
-const mapStoreStateToProps = (state) => {
+const mapStateToProps = (state) => {
   return {
-    expenses: selectExpense(state.expenses, state.filters)
+    expenses: selectExpenses(state.expenses, state.filters)
   };
 };
 
-//combines store and component
-export default connect(mapStoreStateToProps)(ExpenseList);
+export default connect(mapStateToProps)(ExpenseList);
