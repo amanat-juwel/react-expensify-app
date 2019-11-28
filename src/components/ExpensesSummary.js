27df9a86b5931cf338915bf4cbfe64bcaa194ca9
Amanat Juwel <amanatjuwel@gmail.com>
2019-11-28 12:09:17 +0600

    hidden expense count added

--- src/components/ExpensesSummary.js
+++ src/components/ExpensesSummary.js
@@ -3,9 +3,10 @@ import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import numeral from 'numeral';
 import selectExpenses from '../selectors/expenses';
+import selectAllExpenses from '../selectors/all-expenses';
 import selectExpensesTotal from '../selectors/expenses-total';
 
-export const ExpensesSummary = ({ expenseCount, expensesTotal }) => {
+export const ExpensesSummary = ({ expenseCount, expensesTotal, allExpenseCount }) => {
   const expenseWord = expenseCount === 1 ? 'expense' : 'expenses';
   const formattedExpensesTotal = numeral(expensesTotal / 100).format('$0,0.00');
 
@@ -13,6 +14,7 @@ export const ExpensesSummary = ({ expenseCount, expensesTotal }) => {
     <div className="page-header">
       <div className="content-container">
         <h1 className="page-header__title">Viewing <span>{expenseCount}</span> {expenseWord} totalling <span>{formattedExpensesTotal}</span></h1>
+        {(allExpenseCount-expenseCount) > 0 &&  <p>Hidden Expenses: {allExpenseCount-expenseCount}</p>}
         <div className="page-header__actions">
           <Link className="button" to="/create">Add Expense</Link>
         </div>
@@ -23,10 +25,12 @@ export const ExpensesSummary = ({ expenseCount, expensesTotal }) => {
 
 const mapStateToProps = (state) => {
   const visibleExpenses = selectExpenses(state.expenses, state.filters);
+  const allExpenses = selectAllExpenses(state.expenses, state.filters);
 
   return {
     expenseCount: visibleExpenses.length,
-    expensesTotal: selectExpensesTotal(visibleExpenses)
+    expensesTotal: selectExpensesTotal(visibleExpenses),
+    allExpenseCount: allExpenses.length
   };
 };
 
---
+++ src/selectors/all-expenses.js
@@ -0,0 +1,6 @@
+import moment from 'moment';
+// Get visible expenses
+
+export default (expenses, { text, sortBy, startDate, endDate }) => {
+  return expenses;
+};
