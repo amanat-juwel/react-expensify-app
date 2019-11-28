56e1f93d122937a703a890517ebf3346dd8736d6
Amanat Juwel <amanatjuwel@gmail.com>
2019-11-28 13:05:54 +0600

    sweetAlert added

--- src/components/EditExpensePage.js
+++ src/components/EditExpensePage.js
@@ -2,16 +2,53 @@ import React from 'react';
 import { connect } from 'react-redux';
 import ExpenseForm from './ExpenseForm';
 import { startEditExpense, startRemoveExpense } from '../actions/expenses';
-
+import Swal from 'sweetalert2'
+import withReactContent from 'sweetalert2-react-content'
 export class EditExpensePage extends React.Component {
   onSubmit = (expense) => {
     this.props.startEditExpense(this.props.expense.id, expense);
     this.props.history.push('/');
   };
-  onRemove = () => {
+
+  confirmDelete = () => {
     this.props.startRemoveExpense({ id: this.props.expense.id });
     this.props.history.push('/');
   };
+
+  onRemove = () => {
+    const MySwal = withReactContent(Swal)
+
+    MySwal.fire({
+      title: <p>Hello World</p>,
+      footer: 'Copyright 2018',
+      onOpen: () => {
+        // `MySwal` is a subclass of `Swal`
+        //   with all the same instance & static methods
+        MySwal.clickConfirm(<p>Are u sure</p>)
+      },
+      
+    }).then(() => {
+      return MySwal.fire({
+        title: 'Are you sure?',
+        text: "You won't be able to revert this!",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Yes, delete it!'
+      }).then((result) => {
+        if (result.value) {
+          this.confirmDelete();
+          Swal.fire(
+            'Deleted!',
+            'Data has been deleted.',
+            'success'
+          )
+        }
+      })
+    })
+  };
+  
   render() {
     return (
       <div>
